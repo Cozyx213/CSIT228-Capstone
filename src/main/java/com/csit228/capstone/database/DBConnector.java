bd42b20074d6dedb87f9f1c506eae88eed72eea4
Jake <bajentingjackie@gmail.com>
2026-05-03 13:53:31 +0800

    add .env file

---
+++ src/main/java/com/csit228/capstone/database/DBConnector.java
@@ -0,0 +1,41 @@
+package com.csit228.capstone.database;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.sql.Connection;  // Correct JDBC import
+
+public class DBConnector {
+    static String url;
+    static String user;
+    static String password;
+    public static Connection getConnection(){
+        loadEnv();
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("CONNECTED");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("SAD");
+            throw new RuntimeException(e);
+        }
+    }
+    private static void loadEnv(){
+        try(BufferedReader br = new BufferedReader(new FileReader(".env"))){
+            String line;
+//            while((line = br.readLine())!=null){
+//                System.out.println(line);
+//            }
+            url = br.readLine();
+            user= br.readLine();
+            password= br.readLine();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
